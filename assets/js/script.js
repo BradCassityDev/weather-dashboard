fa15ace08a28e9cc3afa5f45ca7cb1422d5b09ae
Brad Cassity <bradley.cassity@gmail.com>
2020-09-06 18:57:07 -0600

    Added fetch statements to grab dynamic endpoint data from OpenWeather API and included icons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,9 @@
 // Creating intial dom objects needed for reference
 var searchBtnEl = document.querySelector("#city-search-btn");
 var searchHistoryContainerEl = document.querySelector("#search-history");
-
+var currentDayCardBodyEl = document.querySelector("#current-day-card-body");
+var fiveDayContainerEl = document.querySelector("#five-day-forcast-container");
+var openWeatherIconUrl = "http://openweathermap.org/img/wn/";
 var apiKey = "e832f3a4b25bedfd65d18bf1a2009124";
 
 // Load search history items from localStorage and parse back to object
@@ -38,8 +40,85 @@ var loadSearchHistory = function() {
     searchHistoryContainerEl.appendChild(historyListEl);
 }
 
+
+
+
 // Call OpenWeather API 
 var callWeatherAPI = function(city) {
+    adjustedCity = city.split(' ').join('+');
+    var currentDate = new Date().getTime() - 1;
+    var cityName, country = "";
+    
+    // Compile URL
+    var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + adjustedCity + 
+    "&appid=e832f3a4b25bedfd65d18bf1a2009124";
+    
+    fetch(apiUrl)
+    .then(function(response) {
+        if(response.ok) {
+            return response.json();
+        } else {
+            console.log("error");
+        }
+    })
+    .then(function(response) {
+        console.log(response);
+        var lat = response.coord.lat;
+        var lon = response.coord.lon;
+        cityName = response.name;
+        country = response.sys.country;
+        
+        var newUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" 
+        + lat + "&lon=" 
+        + lon + "&exclude=minutely,hourly&appid=e832f3a4b25bedfd65d18bf1a2009124";
+                
+         var newUrl = "https://api.openweathermap.org/data/2.5/onecall?units=imperial&lat=" + lat + "&lon=" + lon + "&exclude=minutely,hourly&appid=e832f3a4b25bedfd65d18bf1a2009124"
+        // return the new api call with corresponding lat and long for searched city
+        return fetch(newUrl);
+    })
+    .then(function(response) {
+        if(response.ok) {
+            return response.json();
+        } else {
+            console.log("error");
+        }
+    })
+    .then(function(response) {
+        console.log(response);
+        // Grab and display current date
+        var uvi = response.current.uvi;
+
+        currentDayCardBodyEl.innerHTML = "<h2>" + cityName + ", " + country + " (" + moment.unix(response.current.dt).format("MM/DD/YYYY") + ")" 
+                                        + "<img class='current-day-icon' src='" + openWeatherIconUrl + response.current.weather[0].icon + "@2x.png' /></h2>"
+                                        + "<p>Temperature: " + response.current.temp + " " + String.fromCharCode(176) +"F</p>"
+                                        + "<p>Humidity: " + response.current.humidity + "%</p>"
+                                        + "<p>Wind Speed: " + response.current.wind_speed + " MPH</p>"
+                                        + "<p>UV Index: <span>" + uvi + "</span></p>";
+
+        // Display 5 day Forcast
+        fiveDayContainerEl.innerHTML = "<h4>5-Day Forecast:</h4>";
+        var fiveDayCardGroupEl = document.createElement("div");
+        fiveDayCardGroupEl.classList = "card-deck d-flex justify-content-around";
+
+        for (var i = 1; i <= 5; i++) {
+            var dayCardEl = document.createElement("div");
+            dayCardEl.classList = "card weather-card";
+            var dayCardBodyEl = document.createElement("div");
+            dayCardBodyEl.className = "card-body";
+            dayCardBodyEl.innerHTML = "<p><span class='daily-date'>" + moment.unix(response.daily[i].dt).format("MM/DD/YYYY") + "</span></h4>"
+                                    + "<p><img class='daily-icon' src='" + openWeatherIconUrl + response.daily[i].weather[0].icon + ".png' /></p>"
+                                    + "<p>Temp: " + response.daily[i].temp.day + " " + String.fromCharCode(176) + "F</p>"
+                                    + "<p>Humidity: " + response.daily[i].humidity + "%</p>";
+            
+            dayCardEl.appendChild(dayCardBodyEl);
+            fiveDayCardGroupEl.appendChild(dayCardEl);
+        }
+        fiveDayContainerEl.appendChild(fiveDayCardGroupEl);
+    }) 
+    .catch(function(error) {
+
+    });
+    
     // Update search history with new city
     addCitySearchHistory(city);
 }
