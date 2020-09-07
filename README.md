e80a27d95a8766bfb4505b76ee3d6ed36b64dbfc
Brad Cassity <bradley.cassity@gmail.com>
2020-09-06 21:01:33 -0600

    Adjusted additional styling and added content to the README.md file

---
+++ README.md
@@ -0,0 +1,24 @@
+## Weather Dashboard
+
+## Overview:
+This application allows users to enter a city that is then used to dynamically compile a URL to interact with an OpenWeather API endpoint. Nested fetches allow for the user to type the city name, which is then converted to latitude and longitude that are then used to compiled a second URL to fetch another resource from OpenWeather to grab the current and future forecast for that particular city. The UI design allows users to see all relevant data in one dashboard layout for ease of use. Users can also view a history of their previous 10 searches to quickly reference them again if needed. 
+
+## Built With:
+* HTML
+* CSS
+* Bootstrap
+* Javascript (MomentJS)
+
+## Usage:
+Users can type a city name into the search field to see relevant weather forecasts for both the current and five future days. A search history is saved that contains the users previous 10 searches for ease of use. 
+
+## Other Infomration:
+GitHub Repository: https://github.com/BradCassityDev/weather-dashboard
+
+GitHub Published Site: https://bradcassitydev.github.io/weather-dashboard/
+
+## Before City Search:  
+![Working Project Screenshot](/without-search.PNG)
+
+## After City Search (With Search History):
+![Working Project Screenshot](/with-search.PNG)
--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,6 @@ var searchHistoryContainerEl = document.querySelector("#search-history");
 var currentDayCardBodyEl = document.querySelector("#current-day-card-body");
 var fiveDayContainerEl = document.querySelector("#five-day-forcast-container");
 var openWeatherIconUrl = "http://openweathermap.org/img/wn/";
-var apiKey = "e832f3a4b25bedfd65d18bf1a2009124";
 
 // Load search history items from localStorage and parse back to object
 var searchHistory = JSON.parse(localStorage.getItem("weatherSearchHistory")) || [];
@@ -25,104 +24,132 @@ var addCitySearchHistory = function(city) {
     loadSearchHistory();
 }
 
-
 // Load search history items from local storage
 var loadSearchHistory = function() {
-    searchHistoryContainerEl.innerHTML = "<h5>Last 10 searches</h5>";
+    searchHistoryContainerEl.innerHTML = "<p>Last 10 searches:</p>";
 
+    // Create list group to contain search history items
     var historyListEl = document.createElement("ul");
     historyListEl.className = "list-group";
 
     // Loop through search history array and display results
     for(var i = 0; i < searchHistory.length; i++) {
+        // Create list item
         var searchHistoryItemEl = document.createElement("li");
         searchHistoryItemEl.classList = "list-group-item search-history-items";
         searchHistoryItemEl.textContent = searchHistory[i].cityName;
         searchHistoryItemEl.setAttribute("data-city", searchHistory[i].cityName);
 
+        // Add the list item to the list group
         historyListEl.appendChild(searchHistoryItemEl);
     }
-
+    // Add new search history list to container
     searchHistoryContainerEl.appendChild(historyListEl);
 }
 
-
-
-
 // Call OpenWeather API 
 var callWeatherAPI = function(city) {
-    adjustedCity = city.split(' ').join('+');
-    var currentDate = new Date().getTime() - 1;
+    // get user input to trim and replace all spaces with a '+' for url
+    adjustedCity = city.trim().split(' ').join('+');
     var cityName, country = "";
     
     // Compile URL
     var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + adjustedCity + 
     "&appid=e832f3a4b25bedfd65d18bf1a2009124";
     
+    // Fetch first url api endpoint
     fetch(apiUrl)
     .then(function(response) {
         if(response.ok) {
+             // Response returned with code in 200s
             return response.json();
         } else {
+            // Response returned with code in 400s. Display Error Message
+            currentDayCardBodyEl.innerHTML = "<h4>Something went wrong. Please try again.</h4>";
+            fiveDayContainerEl.innerHTML = "";
             console.log("error");
+            return;
         }
     })
     .then(function(response) {
-        console.log(response);
+        // Capture latitude and longitude to use in next fetch
         var lat = response.coord.lat;
         var lon = response.coord.lon;
         cityName = response.name;
         country = response.sys.country;
         
-        var newUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" 
-        + lat + "&lon=" 
-        + lon + "&exclude=minutely,hourly&appid=e832f3a4b25bedfd65d18bf1a2009124";
-                
-         var newUrl = "https://api.openweathermap.org/data/2.5/onecall?units=imperial&lat=" + lat + "&lon=" + lon + "&exclude=minutely,hourly&appid=e832f3a4b25bedfd65d18bf1a2009124"
+        // Compile new url to use in fetching data from another endpoint
+        var newUrl = "https://api.openweathermap.org/data/2.5/onecall?units=imperial&lat=" 
+            + lat + "&lon=" 
+            + lon 
+            + "&exclude=minutely,hourly&appid=e832f3a4b25bedfd65d18bf1a2009124";
+
         // return the new api call with corresponding lat and long for searched city
         return fetch(newUrl);
     })
     .then(function(response) {
         if(response.ok) {
+            // Response returned with code in 200s
             return response.json();
         } else {
+            // Response returned with code in 400s. Display Error Message
+            currentDayCardBodyEl.innerHTML = "<h4>Something went wrong. Please try again.</h4>";
+            fiveDayContainerEl.innerHTML = "";
             console.log("error");
+            return;
         }
     })
     .then(function(response) {
-        console.log(response);
         // Grab and display current date
         var uvi = response.current.uvi;
+        // Evaluate the UV Index to determine what condition warning should be used
+        if(uvi < 5) {
+            var uviCondition = "bg-success";
+        } else if (uvi >= 5 && uvi < 8) {
+            var uviCondition = "bg-warning";
+        } else {
+            var uviCondition = "bg-danger";
+        }
 
+        // Add Current Day Content to CurrentDayCardBodyEl 
         currentDayCardBodyEl.innerHTML = "<h2>" + cityName + ", " + country + " (" + moment.unix(response.current.dt).format("MM/DD/YYYY") + ")" 
                                         + "<img class='current-day-icon' src='" + openWeatherIconUrl + response.current.weather[0].icon + "@2x.png' /></h2>"
                                         + "<p>Temperature: " + response.current.temp + " " + String.fromCharCode(176) +"F</p>"
                                         + "<p>Humidity: " + response.current.humidity + "%</p>"
                                         + "<p>Wind Speed: " + response.current.wind_speed + " MPH</p>"
-                                        + "<p>UV Index: <span>" + uvi + "</span></p>";
+                                        + "<p>UV Index: <span class='uv-span " + uviCondition + "'>" + uvi + "</span></p>";
 
         // Display 5 day Forcast
         fiveDayContainerEl.innerHTML = "<h4>5-Day Forecast:</h4>";
         var fiveDayCardGroupEl = document.createElement("div");
-        fiveDayCardGroupEl.classList = "card-deck d-flex justify-content-around";
+        fiveDayCardGroupEl.classList = "card-deck justify-content-around";
 
+        // Loop through and display each daily card
         for (var i = 1; i <= 5; i++) {
+            // create card
             var dayCardEl = document.createElement("div");
-            dayCardEl.classList = "card weather-card";
+            dayCardEl.classList = "card weather-card mx-0 mb-3";
+
+            // Create card body
             var dayCardBodyEl = document.createElement("div");
             dayCardBodyEl.className = "card-body";
             dayCardBodyEl.innerHTML = "<p><span class='daily-date'>" + moment.unix(response.daily[i].dt).format("MM/DD/YYYY") + "</span></h4>"
                                     + "<p><img class='daily-icon' src='" + openWeatherIconUrl + response.daily[i].weather[0].icon + ".png' /></p>"
                                     + "<p>Temp: " + response.daily[i].temp.day + " " + String.fromCharCode(176) + "F</p>"
                                     + "<p>Humidity: " + response.daily[i].humidity + "%</p>";
-            
+            // append card body to parent card
             dayCardEl.appendChild(dayCardBodyEl);
+            // append card to five day card group
             fiveDayCardGroupEl.appendChild(dayCardEl);
         }
+        // Append five day card group to container
         fiveDayContainerEl.appendChild(fiveDayCardGroupEl);
     }) 
     .catch(function(error) {
-
+        // Dispaly message if a reponse code in the 500s is returned.
+        console.log(error);
+        currentDayCardBodyEl.innerHTML = "<h4>Something went wrong. Please try again.</h4>";
+        fiveDayContainerEl.innerHTML = "";
     });
     
     // Update search history with new city
@@ -133,6 +160,7 @@ var callWeatherAPI = function(city) {
 var searchCityEvent = function(event) {
     event.preventDefault();
 
+    // get object that was clicked
     var itemClicked = event.target;
 
     if(itemClicked.id === "city-search-btn") {
@@ -153,8 +181,9 @@ var searchCityEvent = function(event) {
     }
 }
 
-
 // Load search history on refresh
 loadSearchHistory();
+
+// Event Listeners for Search Buttn and Search History Items
 searchHistoryContainerEl.addEventListener("click", searchCityEvent);
 searchBtnEl.addEventListener("click", searchCityEvent);
